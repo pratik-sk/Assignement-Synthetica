5d6da28975ea42caed9d3f8a101fabd9210402e1
pratik-sk <pratik.kolapkar@springernature.com>
2017-10-25 11:04:08 +0530

    Changes_for_mobile_view

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//++++++++++++++++Carousal Script++++++++++++++++++++++++++++++//
+
 var slideIndex = 1;
 showSlide(slideIndex);
 
@@ -25,6 +27,7 @@ slides[slideIndex-1].style.display = "block";
 dots[slideIndex-1].className += " active";
 }
 
+//++++++++++++++++++++Navigation Toggle+++++++++++++++++++++++++++++//
 $('#search-btn').click(function() {
     $('.search-bar').toggle();
 });
@@ -38,6 +41,7 @@ $('.nav-icon').click(function(){
     $('.primary-nav-wrapper').toggle();
  });
 
+//+++++++++++++++Search Menu Behaviour+++++++++++++++++++++++//
 $('#search-input').click(function(){
 	this.style.width="70%";
 });
@@ -47,3 +51,92 @@ $('#search-input').keyup(function (e) {
        alert('Ready for implementation')
     }
   });
+
+
+//++++++++++++++++++Counter Increment Script++++++++++++++++++++++++//
+
+function awardCounter(elementName,val,lastVal){
+ var finalval=document.getElementById(elementName);
+
+ finalval.innerHTML=val;
+ val++;
+ 
+ var timeOut=setTimeout("awardCounter('"+elementName+"',"+val+","+lastVal+")",80);
+ if(val==lastVal){
+   finalval.innerHTML=val;
+   clearTimeout(timeOut);
+ }
+}
+awardCounter("awardCount",0,24);
+
+function articleCounter(val){
+ var finalval=document.getElementById("articleCount");
+ finalval.innerHTML=val;
+ val++;
+ var timeOut=setTimeout("articleCounter("+val+")",2);
+ if(val==341){
+   finalval.innerHTML=val;
+   clearTimeout(timeOut);
+ }
+}
+articleCounter(0);
+
+function freebiesCounter(val){
+ var finalval=document.getElementById("freebiesCount");
+ finalval.innerHTML=val;
+ val++;
+ var timeOut=setTimeout("freebiesCounter("+val+")",40);
+ if(val==43){
+   finalval.innerHTML=val;
+   clearTimeout(timeOut);
+ }
+}
+freebiesCounter(0);
+
+//+++++++++++++++++Progress Bar Scrip++++++++++++++++++++++++++++++++//
+function progressBar(val){
+ var expDesignCountBar=document.getElementById("expDesignCountBar");
+ var interactivePrototypeCountBar=document.getElementById("interactivePrototypeCountBar");
+ var htmlSkillCountBar=document.getElementById("htmlSkillCountBar");
+ var visulaDesignCountBar=document.getElementById("visulaDesignCountBar");
+ 
+ expDesignCountBar.value=val;
+ interactivePrototypeCountBar.value=val;
+ htmlSkillCountBar.value=val;
+ visulaDesignCountBar.value=val;
+ 
+ val++;
+ 
+ var timeOut=setTimeout("progressBar("+val+")",20);
+ if(val==85){
+   expDesignCountBar.value=90;
+   interactivePrototypeCountBar.value=80;
+   htmlSkillCountBar.value=95;
+   visulaDesignCountBar.value=90;
+   clearTimeout(timeOut);
+ }
+}
+progressBar(0);
+
+//++++++++++++++++++++Social Share nav+++++++++++++++++++++++++++//
+
+$('#social-share').click(function(){
+	$(".social-media-share").toggle();
+});
+
+//++++++++++++++++++++++Navigate to Div++++++++++++++++++++++++//
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    },1000);
+});
+
+$(".footer-primary-nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    },1000);
+});
